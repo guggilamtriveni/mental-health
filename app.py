6f3432e23695e8c59af79562e5f2c828749a457e
guggilamtriveni <triveniguggilam1@gmail.com>
2026-01-28 16:40:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load saved files
